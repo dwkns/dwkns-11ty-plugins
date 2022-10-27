0a99577e2e8e0653c38abaac5519bd99a8c99b2f
dwkns <dwkns@me.com>
2022-10-27 16:26:43 +0100

    Improve data parsing

--- plugins/readableDate.js
+++ plugins/readableDate.js
@@ -1,12 +1,13 @@
-const { DateTime } = require("luxon");
+const { DateTime } = require('luxon');
 
 // Prepend 'A' or 'An' depeding on the next word supplied
 const readableDate = (eleventyConfig) => {
-  eleventyConfig.addFilter("readableDate",(dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
-  })
-}
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    const readableDate = DateTime.fromISO(dateObj).toFormat('dd LLL yyyy');
+    return readableDate;
+  });
+};
 
 module.exports = {
   readableDate,
-}
+};
